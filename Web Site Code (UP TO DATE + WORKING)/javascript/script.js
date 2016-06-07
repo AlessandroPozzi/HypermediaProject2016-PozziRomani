c6240b6d2752af3feb25786ccf558b89dbde5246
AlessandroPozzi <pozzi.ale93@gmail.com>
2016-06-07 16:57:59 +0200

    The HOLY COMMIT

--- Web Site Code (UP TO DATE + WORKING)/javascript/script.js
+++ Web Site Code (UP TO DATE + WORKING)/javascript/script.js
@@ -30,24 +30,29 @@ function loadMoreClicked(){
     });
     */
     
-    /* WITH JSON, WORKING */
+    /* WITH JSON, WORKING  */
     $.ajax({
         method: "POST",
         //dataType: "json", //type of data
-        url: "http://timhypermediaproject2016.altervista.org/php/getDescription.php", 
+        url: "http://timhypermediaproject2016.altervista.org/php/getDescription.php",
+        data: {name:"Apple iPhone 6s 16GB"},
         success: function(response) {
 			
         	console.log("Ajax call: success!");
-            console.log(JSON.parse(response));
-
+            var json = JSON.parse(response);
             
-            $("#result").html(" "+response);
+            console.log(JSON.parse(response));
+            //console.log("JAVASCRIPT REPORTING: JSON IS HERE: " + JSON.parse(response));
+            //console.log( unescape(json[0].descrizione.replace(/\+/g, '%20')));//----
+            
+             //$("#result").html(response);
+            $("#result").html(" "+ json[7].name);
         },
         error: function(request,error) 
         {
             console.log("Error with the ajax call");
         }
     });
-    
+   
 
 }
--- Web Site Code (UP TO DATE + WORKING)/php/getDescription.php
+++ Web Site Code (UP TO DATE + WORKING)/php/getDescription.php
@@ -4,10 +4,14 @@
 getDescription();
 //echo $queryResult;
 
-function getDescription($pt){
-    
+function getDescription($name){
+    mysql_query("SET NAMES 'utf8_bin'");
+            mysql_query("SET CHARACTER SET utf8_bin");
+            mysql_query("SET COLLATION_CONNECTION = 'utf8mb4_bin'");
     $mysqli = new mysqli("localhost", "timhypermediaproject2016", "", "my_timhypermediaproject2016");
+    //mysql_query('SET CHARACTER SET utf8');
     //sql query
+    
     if (mysqli_connect_errno()){
         echo "Error".mysqli_connect_error();
         exit();
@@ -15,20 +19,39 @@ function getDescription($pt){
     else{
         //echo "successufl connection";
             
+            //$query = "SELECT name,description,price" FROM "device" WHERE ;
+            //mysql_query('SET CHARACTER SET utf8');
+            //mysql_query("SET NAMES latin1");
+            mysql_query("SET NAMES 'utf8_bin'");
+            mysql_query("SET CHARACTER SET utf8_bin");
+            mysql_query("SET COLLATION_CONNECTION = 'utf8mb4_bin'");
             $query = "SELECT * FROM device";
-
+			//$query = "SELECT * FROM test_table";
+            //mysqli_query($result,"SELECT * FROM test_table");
+            //$query ="SELECT category FROM high_level_categories WHERE macro_group=DISPOSITIVI";
+			
             $result = $mysqli->query($query);
 
             if($result->num_rows>0)
             {
                 $myArray = array();
                 while($row = $result->fetch_array(MYSQL_ASSOC)){
-                    $myArray[] = $row;
-                    //echo $row[name];
+                   //echo $row[name];
+                   //echo $row[descr];
+                   
+                    //$myArray[] = $row;
+                    
+                    $myArray[] = array_map('utf8_encode', $row);
                 }
+
                 //echo "OK";
                 //echo $myArray;
-	  			echo json_encode($myArray);
+                //$somearray['mystring'] = utf8_encode($mystring);
+                //echo json_encode($myArray);
+               
+	  			echo json_encode($myArray, JSON_UNESCAPED_UNICODE );
+                //echo json_last_error ();
+                //echo json_encode("{[àààààèèèèèòòòò]}",JSON_UNESCAPED_UNICODE);
             }
             //free result
             $result->close();
