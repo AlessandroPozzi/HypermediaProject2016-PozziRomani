286ad0dc578e12e738d887da92bfb5f1339e90c0
Marco <marcoevans93@gmail.com>
2016-06-09 18:33:51 +0200

    group pages WORKING

--- Web Site Code (UP TO DATE + WORKING)/php/getCategories2.php
+++ Web Site Code (UP TO DATE + WORKING)/php/getCategories2.php
@@ -1,10 +1,14 @@
 <?php
 
-       getElementOfCategory();
+       getCategories2();
 		
-       function getElementOfCategory(){
-       	$catX = $_GET['catX'];
-		
+       function getCategories2(){
+       	$high_cat = $_GET['high_cat'];
+		   $doc = new DOMDocument();
+           $doc->loadHTMLFile("../pages/categories.html");
+		   $xpathsearch = new DOMXPath($doc);
+           $nodes = $xpathsearch->query('//div[contains(@class,"result")]'); 
+           //echo $doc->saveHtml();
 		   
           $mysqli = new mysqli("localhost", "timhypermediaproject2016", "", "my_timhypermediaproject2016");
         //sql query
@@ -13,37 +17,22 @@
             exit();
          }
          else{
-
-             $query1 ="SELECT content FROM device_categories_content WHERE device_category="."'".$catX."'";
-			 $query2 ="SELECT content FROM sl_categories_content WHERE sl_category="."'".$catX."'";
-			 $query3 ="SELECT content FROM assistance_categories_content WHERE assistance_category="."'".$catX."'";
+		    $category="category";
+			 $high_level_categories="high_level_categories";
+			 $macro_group="macro_group";
+			 
+             //echo $high_cat;
+             //$query ="SELECT category FROM high_level_categories WHERE macro_group='DISPOSITIVI'";//.$high_cat;
+			 
+             $query ="SELECT category FROM high_level_categories WHERE macro_group="."'".$high_cat."'";
 			 
 
-             $result1 = $mysqli->query($query1);
-			 $result2 = $mysqli->query($query2);
-			 $result3 = $mysqli->query($query3);
-			 
-		     manipulateResults($result1,$mysqli);
-			 manipulateResults($result2,$mysqli);
-			 manipulateResults($result3,$mysqli);
+             $result = $mysqli->query($query);
+		     
 			
-
-            //close connection
-            $mysqli->close();
-            
-		 }
-       }
-	   
-	   function manipulateResults($result,$mysqli){
-		   
-		   $doc = new DOMDocument();
-           $doc->loadHTMLFile("../pages/categories.html");
-		   $xpathsearch = new DOMXPath($doc);
-           $nodes = $xpathsearch->query('//div[contains(@class,"result")]'); 
-		   
-		   
-		   
-		   if($result->num_rows>0){
+			 
+			 
+			 if($result->num_rows>0){
                 $myArray = array();
                 while($row = $result->fetch_array(MYSQL_ASSOC)){
                     $myArray[] = $row;
@@ -51,15 +40,13 @@
                 }
 				
 			   foreach($nodes as $node){
-				   $acc=array();
-				   $count=0;
+                   $acc=array();
+				   
 				  foreach($myArray as $elem){
-					$count = $count + 1; 
 					$newnode = $doc->createDocumentFragment();
                 
-                
-					$content=$elem['content'];
-					$queryimg="SELECT url FROM content_images WHERE content="."'".$content."'";
+					$cat=$elem['category'];
+					$queryimg="SELECT url FROM content_images WHERE content="."'".$cat."'";
 					$resultimg=$mysqli->query($queryimg);
 					$arrayresultimg = array();
                     while($rowimg = $resultimg->fetch_array(MYSQL_ASSOC)){
@@ -75,28 +62,28 @@
 					
 					$codeimg = "<img class='img-responsive' src='".$url."'/>";
 					
-					$codefooter = "<div class="."'panel-footer high_cat'".">{$elem['content']}</div>";
+					$codefooter = "<div class="."'panel-footer high_cat'".">{$elem['category']}</div>";
                     $codeimgandfooter = $codeimg.$codefooter;
                     $codeprimary ="<div class= 'panel panel-primary'>{$codeimgandfooter}</div>";
-                    $code= "<div class="."'col-sm-3'".">{$codeprimary}</div>";
+                    $code= "<div class="."'col-sm-4 col-lg-3  col-centered clearfix'".">{$codeprimary}</div>";
 
-                   // $newnode->appendXML($code);
+                  
                     array_push($acc,$code);
 					
-					if(sizeof($acc)==4){
+					if(sizeof($acc)==3){
 						$tags="";
 						foreach($acc as $col){
 							$tags=$tags.$col;
 						}
-						$rowcode="<div class="."'row'".">{$tags}</div>";
+						$rowcode="<div class="."'row row-centered'".">{$tags}</div>";
 						$newnode->appendXML($rowcode);
 						$node->appendChild($newnode);
 						$acc=array();
 					}
-				
+			
 					
 				 }
-				 if(sizeof($acc)>0){
+                 if(sizeof($acc)>0){
 					 $tags="";
 						foreach($acc as $col){
 							$tags=$tags.$col;
@@ -110,8 +97,17 @@
               echo $doc->saveHtml();	
               
               
-			 }
-	   }
+			 }  else{
+             echo "no rows";
+             }
+			   //free result
+            //$result->close();
+
+            //close connection
+            $mysqli->close();
+            
+		 }
+       }
 
 
      ?>
